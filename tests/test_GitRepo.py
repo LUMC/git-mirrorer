25eed55d0c6ddc8ae2467bea70a9993f036cda4c
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-04-12 16:39:05 +0200

    start on test

--- tests/test_GitRepo.py
+++ tests/test_GitRepo.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with git-synchronizer.  If not, see <https://www.gnu.org/licenses/
 
+import os
 import tempfile
 from pathlib import Path
 
@@ -41,4 +42,7 @@ def test_clone(git_repository):
     git_repo.clone()
     assert git_repo.repo_dir.exists()
 
-
+def test_fetch(git_repository):
+    git_repo = git_repository
+    git_repo.clone()
+    git_repo.fetch()
