173154b81cca522bb547b594b060c803dba76c77
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-04-12 15:25:42 +0200

    write fixture

--- tests/test_GitRepo.py
+++ tests/test_GitRepo.py
@@ -14,5 +14,23 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with git-synchronizer.  If not, see <https://www.gnu.org/licenses/
 
+import tempfile
+from pathlib import Path
 from git_synchronizer.git_synchronizer import GitRepo
+
+import pytest
+
+
+@pytest.fixture()
+def git_repo():
+    main_git = "https://github.com/LUMC/git-synchronizer.git"
+    git_mirror_1 = str(Path(tempfile.mkdtemp(suffix=".git", prefix="mirror1")))
+    git_mirror_2 = str(Path(tempfile.mkdtemp(suffix=".git", prefix="mirror2")))
+    clone_dir = Path(str(tempfile.mkdtemp(prefix="clone_dir"))) / Path(
+        "git-synchronizer.git")
+    git_repo = GitRepo(main_url=main_git,
+                       mirror_urls=[git_mirror_1, git_mirror_2],
+                       repo_dir=Path(str(clone_dir)) / Path(
+                           "git_synchronizer.git"))
+    return git_repo, git_mirror_1, git_mirror_2, clone_dir
 
