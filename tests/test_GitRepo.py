2cf229f8b010c727180568531590a5e179f97b60
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-04-12 15:12:56 +0200

    license test files

--- tests/test_GitRepo.py
+++ tests/test_GitRepo.py
@@ -14,18 +14,5 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with git-synchronizer.  If not, see <https://www.gnu.org/licenses/
 
-from pathlib import Path
+from git_synchronizer.git_synchronizer import GitRepo
 
-from git_synchronizer.git_synchronizer import parse_config
-
-EXAMPLE_CONFIG = Path(__file__).parent.parent / Path("example_config.tsv")
-
-
-def test_config_parsing():
-    config = parse_config(EXAMPLE_CONFIG)
-    assert len(config) == 2
-    assert config[0] == ("https://example.com/examples/example.git",
-                         ["git@mygit.com:/examples/example.git"])
-    assert config[1] == ("https://example.com/examples/example2.git",
-                         ["git@mygit.com:/examples/example2.git",
-                          "git@myothergit.com/example/example2.git"])
